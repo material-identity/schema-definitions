62c658c576cb278fc0522a874ca32b88daf943bc
Eamon <eamon0989@gmail.com>
2022-12-14 14:36:11 +0100

    test(chemical-element): update tests to account for changes

--- chemical-element/test/test-suites-map.js
+++ chemical-element/test/test-suites-map.js
@@ -14,35 +14,25 @@ const invalidCertTestSuitesMap = [
     certificateName: 'invalid-certificate-1',
     expectedErrors: [
       {
-        instancePath: '/Certificate/ChemicalElement/Maximum',
+        instancePath: '/Certificate/ChemicalElement/Actual',
         schemaPath:
-          'chemical-element.json#/definitions/ChemicalElement/properties/Maximum/maximum',
-        keyword: 'maximum',
-        params: { comparison: '<=', limit: 100 },
-        message: 'must be <= 100',
+          'chemical-element.json#/definitions/ChemicalElement/properties/Actual/type',
+        keyword: 'type',
+        params: { type: 'string' },
+        message: 'must be string',
       },
-    ],
-  },
-  {
-    certificateName: 'invalid-certificate-2',
-    expectedErrors: [
       {
         instancePath: '/Certificate/ChemicalElement/Minimum',
         schemaPath:
-          'chemical-element.json#/definitions/ChemicalElement/properties/Minimum/minimum',
-        keyword: 'minimum',
-        params: { comparison: '>=', limit: 0 },
-        message: 'must be >= 0',
+          'chemical-element.json#/definitions/ChemicalElement/properties/Minimum/type',
+        keyword: 'type',
+        params: { type: 'string' },
+        message: 'must be string',
       },
-    ],
-  },
-  {
-    certificateName: 'invalid-certificate-3',
-    expectedErrors: [
       {
-        instancePath: '/Certificate/ChemicalElement/Symbol',
+        instancePath: '/Certificate/ChemicalElement/Maximum',
         schemaPath:
-          'chemical-element.json#/definitions/ChemicalElement/properties/Symbol/type',
+          'chemical-element.json#/definitions/ChemicalElement/properties/Maximum/type',
         keyword: 'type',
         params: { type: 'string' },
         message: 'must be string',
@@ -50,67 +40,28 @@ const invalidCertTestSuitesMap = [
     ],
   },
   {
-    certificateName: 'invalid-certificate-4',
+    certificateName: 'invalid-certificate-2',
     expectedErrors: [
       {
-        instancePath: '/Certificate/ChemicalElement/Actual',
+        instancePath: '/Certificate/ChemicalElement',
         schemaPath:
-          'chemical-element.json#/definitions/ChemicalElement/properties/Actual/maximum',
-        keyword: 'maximum',
-        params: { comparison: '<=', limit: 100 },
-        message: 'must be <= 100',
+          'chemical-element.json#/definitions/ChemicalElement/required',
+        keyword: 'required',
+        params: { missingProperty: 'Symbol' },
+        message: "must have required property 'Symbol'",
       },
     ],
   },
   {
-    certificateName: 'invalid-certificate-5',
+    certificateName: 'invalid-certificate-3',
     expectedErrors: [
       {
-        instancePath: '/Certificate/ChemicalElement/Actual',
+        instancePath: '/Certificate/ChemicalElement',
         schemaPath:
-          'chemical-element.json#/definitions/ChemicalElement/properties/Actual/minimum',
-        keyword: 'minimum',
-        params: { comparison: '>=', limit: 0 },
-        message: 'must be >= 0',
-      },
-    ],
-  },
-  {
-    certificateName: 'invalid-certificate-6',
-    expectedErrors: [
-      {
-        instancePath: '/Certificate/ChemicalElement/Actual',
-        schemaPath:
-          'chemical-element.json#/definitions/ChemicalElement/properties/Actual/type',
-        keyword: 'type',
-        params: { type: 'number' },
-        message: 'must be number',
-      },
-    ],
-  },
-  {
-    certificateName: 'invalid-certificate-7',
-    expectedErrors: [
-      {
-        instancePath: '/Certificate/ChemicalElement/Maximum',
-        schemaPath:
-          'chemical-element.json#/definitions/ChemicalElement/properties/Maximum/type',
-        keyword: 'type',
-        params: { type: 'number' },
-        message: 'must be number',
-      },
-    ],
-  },
-  {
-    certificateName: 'invalid-certificate-8',
-    expectedErrors: [
-      {
-        instancePath: '/Certificate/ChemicalElement/Minimum',
-        schemaPath:
-          'chemical-element.json#/definitions/ChemicalElement/properties/Minimum/type',
-        keyword: 'type',
-        params: { type: 'number' },
-        message: 'must be number',
+          'chemical-element.json#/definitions/ChemicalElement/required',
+        keyword: 'required',
+        params: { missingProperty: 'Actual' },
+        message: "must have required property 'Actual'",
       },
     ],
   },
